916a04e007c1a34578b2175067a444ae5a7eaafb
Administrator <1>
2019-01-11 08:58:03 +0800

    1

--- header.php
+++ header.php
@@ -2,9 +2,7 @@
 header("Access-Control-Allow-Credentials:true");
 header('Content-type:text/json');
 header('Access-Control-Allow-Headers:Content-Type');
-$allow_origin=array(
-    'http://10.30.29.67:63341'
-);
+header("Access-Control-Allow-Origin:http://10.30.29.67:63341");
 $origin=isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:'';
 if(in_array($origin, $allow_origin)) {
     header('Access-Control-Allow-Origin:' . $origin);
